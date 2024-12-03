5dfb734c523997b4dec785aaeed65abe5021a56e
gabimezze <gabrielagomescezar@gmail.com>
2024-12-03 15:39:27 -0300

    renomeando headers

--- my-portfolio/src/components/Header/HomeHeader.tsx
+++ my-portfolio/src/components/Header/HomeHeader.tsx
@@ -1,13 +1,13 @@
 import logo from '../../img/logo-mezze.png'
 import '../../estilo.css'
-export default function Sobre() {
+export default function HomeHeader() {
     return (
         <header>
             <img src={logo} alt="logo-mezze" />
             <nav>
                 <ul>
-                    <li className="link"><a href="/"></a>Home</li>
-                    <li className="link"><a className='active' href="/sobre"></a>Sobre mim</li>
+                    <li className="link"><a className='active' href="/"></a>Home</li>
+                    <li className="link"><a href="/sobre"></a>Sobre mim</li>
                     <li className="link"><a href="/projetos"></a>Projetos</li>
                 </ul>
             </nav>
--- my-portfolio/src/components/Header/SobreHeader.tsx
+++ my-portfolio/src/components/Header/SobreHeader.tsx
@@ -1,14 +1,14 @@
 import logo from '../../img/logo-mezze.png'
 import '../../estilo.css'
-export default function Projetos() {
+export default function SobreHeader() {
     return (
         <header>
             <img src={logo} alt="logo-mezze" />
             <nav>
                 <ul>
                     <li className="link"><a href="/"></a>Home</li>
-                    <li className="link"><a href="/sobre"></a>Sobre mim</li>
-                    <li className="link"><a className='active' href="/projetos"></a>Projetos</li>
+                    <li className="link"><a className='active' href="/sobre"></a>Sobre mim</li>
+                    <li className="link"><a href="/projetos"></a>Projetos</li>
                 </ul>
             </nav>
         </header>
