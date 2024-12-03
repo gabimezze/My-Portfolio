6a9e7335280cd6c6e39abe717b0c4812cd2dd6fb
gabimezze <gabrielagomescezar@gmail.com>
2024-12-03 15:47:57 -0300

    editando links routes

--- my-portfolio/src/routes/routes.tsx
+++ my-portfolio/src/routes/routes.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './home/index';
-import Sobre from './sobre';
-import Projetos from './projetos';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./home/index";
+import Sobre from "./sobre/index";
+import Projetos from "./projetos/index";
 
-export default function AppRoutes () {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/sobre" element={<Sobre />} />
-                <Route path="/projetos" element={<Projetos />} />
-            </Routes>
-        </Router>
-    );
-};
+export default function AppRoutes() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sobre" element={<Sobre />} />
+        <Route path="/projetos" element={<Projetos />} />
+      </Routes>
+    </Router>
+  );
+}
