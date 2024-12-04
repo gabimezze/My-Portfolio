1049d232ae7de7a4f95b54f0393612b3aa5db59a
gabimezze <gabrielagomescezar@gmail.com>
2024-12-03 21:53:14 -0300

    update tamanho img

--- my-portfolio/src/components/Cards/CardPerfil.tsx
+++ my-portfolio/src/components/Cards/CardPerfil.tsx
@@ -3,7 +3,7 @@ import perfil from '../../img/perfil.jpg'
 export default function CardPerfil() {
     return (
         <div className='img-container'>
-            <img src={perfil} width={320} height={320} alt="foto-gabriela" />
+            <img src={perfil} width={400} height={400} alt="foto-gabriela" />
         </div>
     )
 }
