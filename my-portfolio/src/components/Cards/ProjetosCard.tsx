1fde79562b97dd5c8dc7bd6ed0c907a33ad680c9
gabimezze <gabrielagomescezar@gmail.com>
2024-12-04 20:00:31 -0300

    update card projetos

--- my-portfolio/src/components/Cards/ProjetosCard.tsx
+++ my-portfolio/src/components/Cards/ProjetosCard.tsx
@@ -6,23 +6,23 @@ export default function ProjetosCard () {
 <div className="p-container">
             <h1>Projetos</h1>
             <div className="pcard">
+                <a href="https://blueoceann.netlify.app/"><img src="/src/img/blueocean.png" width={290} height={150} alt="blueocean" /></a>
                 <h2>BlueOcean</h2>
-                <a href="https://blueoceann.netlify.app/"><img src="https://blueoceann.netlify.app/" alt="blueocean" /></a>
                 <p>Projeto Front End para a conscientização da poluição marinha.</p>
             </div>
             <div className="pcard">
+                <a href=""><img src="src/img/portfolio.png" width={290} height={150} alt="portfolio" /></a>
                 <h2>Portfólio</h2>
-                <a href=""><img src="]" alt="" /></a>
                 <p>Projeto Front End para apresentar minhas habilidades.</p>
             </div>
             <div className='pcard'>
+                <a href="https://caseportofiap.netlify.app/"><img src="/src/img/porto.png" width={290} height={150} alt="porto" /></a>
                 <h2>Porto Seguro</h2>
-                <a href="https://caseportofiap.netlify.app/"><img src="" alt="" /></a>
                 <p>Case da faculdade Fiap com a Porto Seguro.</p>
             </div>
             <div className="pcard">
+                <a href=""><img src="/src/img/portavaliacoes.png" width={290} height={150} alt="Portofólio Avaliações" /></a>
                 <h2>Portfólio de Avaliações</h2>
-                <a href=""></a>
                 <p>Projeto Full-Stack para mostrar notas dos alunos.</p>
             </div>
             </div>
