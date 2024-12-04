d1966ed7784ea8fec7310cf523b5551fa01e4e29
gabimezze <gabrielagomescezar@gmail.com>
2024-12-04 17:45:16 -0300

    add import card projetos

--- my-portfolio/src/routes/projetos/index.tsx
+++ my-portfolio/src/routes/projetos/index.tsx
@@ -1,13 +1,14 @@
 import ProjetosHeader from "../../components/Header/ProjetosHeader";
 import Rodape from "../../components/Rodape/Rodape";
+import ProjetosCard from "../../components/Cards/ProjetosCard";
 import '../../estilo.css'
 
 export default function Projetos() {
     return (
         <>
-            <ProjetosHeader />
-            <h1>Projetos</h1>
-            <Rodape />
+        <ProjetosHeader />
+        <ProjetosCard />
+        <Rodape />
         </>
     );
 };
