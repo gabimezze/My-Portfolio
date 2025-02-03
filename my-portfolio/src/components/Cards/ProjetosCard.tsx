8d30035e0d42f56c6aab8e2c9a5071b21b23d5f6
gabimezze <gabrielagomescezar@gmail.com>
2025-02-03 01:03:02 -0300

    update import img

--- my-portfolio/src/components/Cards/ProjetosCard.tsx
+++ my-portfolio/src/components/Cards/ProjetosCard.tsx
@@ -1,24 +1,24 @@
 import '../../style/estilo.css'
-import '../../img/blueocean.png'
-import '../../img/portfolio.png'
-import '../../img/porto.png'
+import blue from '../../img/blueocean.png'
+import port from '../../img/portfolio.png'
+import porto from '../../img/porto.png'
 
 export default function ProjetosCard () {
     return (
 <div className="p-container">
             <h1>Projetos</h1>
             <div className="pcard">
-                <a href="https://blueoceann.netlify.app/"><img src='src/img/blueocean.png' width={290} height={150} alt="blueocean" /></a>
+                <a href="https://blueoceann.netlify.app/"><img src={blue} width={290} height={150} alt="blueocean" /></a>
                 <h2>BlueOcean</h2>
                 <p>Projeto Front End para a conscientização da poluição marinha.</p>
             </div>
             <div className="pcard">
-                <a href="https://mezzedev.vercel.app/"><img src="src/img/portfolio.png" width={290} height={150} alt="portfolio" /></a>
+                <a href="https://mezzedev.vercel.app/"><img src={port} width={290} height={150} alt="portfolio" /></a>
                 <h2>Portfólio</h2>
                 <p>Projeto Front End para apresentar minhas habilidades.</p>
             </div>
             <div className='pcard'>
-                <a href="https://challengefiap.vercel.app/"><img src="src/img/porto.png" width={290} height={150} alt="porto" /></a>
+                <a href="https://challengefiap.vercel.app/"><img src={porto} width={290} height={150} alt="porto" /></a>
                 <h2>Porto Seguro</h2>
                 <p>Case da faculdade Fiap com a Porto Seguro.</p>
             </div>
